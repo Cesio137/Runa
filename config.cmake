09ad1aa080a4d8287d30f0d98a0debd3039b83be
ΣRRΩR 404 <nathan.cm77@gmail.com>
2023-01-12 21:00:24 -0300

    added build.bat files

---
+++ config.cmake
@@ -0,0 +1,11 @@
+#Configure thirdparty libraries
+add_subdirectory(thirdparty)
+
+#Project Configuration
+#[[
+set(ENV{BUILD_SHARED_LIBS} ON)
+set(ENV{GLFW_BUILD_EXAMPLES} OFF)
+set(ENV{GLFW_BUILD_TESTS} OFF)
+set(ENV{GLFW_BUILD_DOCS} OFF)
+set(ENV{GLFW_INSTALL} OFF)
+]]

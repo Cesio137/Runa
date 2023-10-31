3ef31c85b7fe2e269a02bb89204ec8e301c3b4e6
Nathan Miguel <nathan.cm77@gmail.com>
2023-10-31 13:16:23 -0300

    Delete source directory

--- source/data/public/type/Types.h
+++
@@ -1,8 +0,0 @@
-#ifndef TYPE_H
-#define TYPE_H
-
-//#include <QString>
-
-//typedef QString FString;
-
-#endif // TYPE_H
--- source/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include <Core.h>
-
-int main(int argc, char *argv[])
-{
-    Core a(argc, argv);
-    
-    return a.exec();;
-}

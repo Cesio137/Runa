860af6338c988069f4419ded554e2425a41cbce2
Nathan Miguel <nathan.cm77@gmail.com>
2024-05-11 01:01:36 -0300

    Updated some size info

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main(int argc, char* argv[])
 {
     Nanometro::RenderInterface RHI(OPENGL_INIT_330);
     int code = RHI.exec();
+    size_t size = sizeof(RHI);
+    std::cout << size << "\n";
     if (!code)
         std::cout << RHI.GetErrorLog() << "\n";
-
     return code;
 }

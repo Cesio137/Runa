f28825d4818115d9041cd1c0276df3b2c9c66eee
Nathan Miguel <nathan.cm77@gmail.com>
2024-11-27 14:18:56 -0300

    Updated imgui

--- Engine/Source/Thirdparty/Thirdparty.cmake
+++ Engine/Source/Thirdparty/Thirdparty.cmake
@@ -2,7 +2,6 @@ set(THIRDPARTY_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 # Graphics API
 add_subdirectory(${THIRDPARTY_DIR}/vulkan)
-add_subdirectory(${THIRDPARTY_DIR}/opencl)
 # User Interface
 add_subdirectory(${THIRDPARTY_DIR}/imgui)
 

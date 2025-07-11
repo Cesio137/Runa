40c82e27e468a2c50f780fb2019e2054d8cbf9e7
Nathan Miguel <nathan.cm77@gmail.com>
2025-07-10 23:01:41 -0300

    updated lib versions

--- engine/thirdparty/luau/xmake.lua
+++ engine/thirdparty/luau/xmake.lua
@@ -5,7 +5,7 @@ package("luau")
     set_homepage("https://luau.org/")
     set_description("Luau")
     add_urls("https://github.com/luau-lang/luau.git")
-    add_versions("0.679", "0.679")
+    add_versions("0.681", "0.681")
 
     on_install(function (package)
         -- Luau configs
